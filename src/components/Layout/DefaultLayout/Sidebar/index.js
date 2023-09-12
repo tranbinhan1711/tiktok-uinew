385b5c61409171b9560d9ea91f47b7345e665007
tranbinhan1711 <antran171159@gmail.com>
2023-09-12 17:09:14 +0700

    Ngày 2,3(buổi chiều)

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,4 +1,10 @@
+import styles from './Sidebar.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return <aside className={cx('wrapper')}> 
+                <h2>Sidebar</h2>
+    </aside>
 }
  export default Sidebar;
